8f09afe4568c616967b777a64e99ffec6c2da74f
Helder Correia <helder@19labs.com>
2020-08-23 00:09:41 +0100

    SW WIP

---
+++ workbox-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  globDirectory: ".",
+  globPatterns: [
+    "**/*.{css,png,html,js}"
+  ],
+  swDest: "sw.js",
+  clientsClaim: true,
+  maximumFileSizeToCacheInBytes: 5242880
+};
